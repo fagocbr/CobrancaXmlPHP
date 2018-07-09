0723a7934cfad920a25788038ff750d0d21f6ca4
William Correa <wilcorrea@gmail.com>
2018-07-09 13:30:23 -0300

    Redirecionar saída do docker-compose para &> /dev/null

--- composer.test.sh
+++ composer.test.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 CONTAINER_NAME="cobrancaxml.php-app"
+
 if [[ ! $(docker ps -q -f name=${CONTAINER_NAME}) ]]; then
-  docker-compose up -d
+  docker-compose up -d &> /dev/null
 fi
 docker exec ${CONTAINER_NAME} vendor/bin/phpunit --colors=always
 
-docker-compose down
+docker-compose down &> /dev/null
